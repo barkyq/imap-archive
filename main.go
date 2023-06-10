1aab406f780de9ffbb35a1673072c13a65144337
Dylan Cant <dylan@dylancant.ca>
2023-06-09 22:50:22 -0400

    use storage by hash id
    ID is hash of headers

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"os"
 	"path/filepath"
 
+	"bufio"
+	"crypto/sha256"
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
-	"github.com/emersion/go-maildir"
 )
 
 var continue_flag = flag.Int("c", 0, "index from which to resume")
@@ -21,33 +22,20 @@ func main() {
 	if e != nil {
 		panic(e)
 	}
-
+	targetdir := filepath.Join(directory, "offline")
 	c, e := client.DialTLS(addr, nil)
 	if e != nil {
 		panic(e)
 	}
 	defer c.Logout()
-	// if _, y, e := a.Start(); e == nil {
-	// 	fmt.Println(base64.StdEncoding.EncodeToString(y))
-	// }
 	if e := c.Authenticate(a); e != nil {
 		panic(e)
 	}
-	// if cap, e := c.Capability(); e != nil {
-	// 	panic(e)
-	// } else {
-	// 	for k, v := range cap {
-	// 		fmt.Println(k, v)
-	// 	}
-	// }
 	ch := make(chan *imap.MailboxInfo)
 	folder_chan := make(chan []string)
 	go func() {
 		folders := make([]string, 0, 128)
 		for i := range ch {
-			if e := os.MkdirAll(filepath.Join(directory, i.Name), os.ModePerm); e != nil {
-				panic(e)
-			}
 			folders = append(folders, i.Name)
 		}
 		folder_chan <- folders
@@ -60,13 +48,13 @@ func main() {
 	donechan := make(chan struct{}, 1)
 	donechan <- struct{}{}
 	for folders := range folder_chan {
-		for k, f := range folders {
-			fmt.Println(k, f)
+		for k, folder := range folders {
+			fmt.Println(k, folder)
 			if k < *continue_flag {
 				continue
 			}
 			<-donechan
-			if stat, e := c.Select(f, true); e != nil {
+			if stat, e := c.Select(folder, true); e != nil {
 				panic(e)
 			} else {
 				section := &imap.BodySectionName{Peek: true}
@@ -80,42 +68,58 @@ func main() {
 				}
 				fetch_chan := make(chan *imap.Message)
 				go func() {
-					for msg := range fetch_chan {
-						if message, e := mail.ReadMessage(msg.GetBody(section)); e != nil {
+					hash := sha256.New()
+					var first_byte string
+					var rest_bytes string
+					var digest [32]byte
+					var header mail.Header
+					var msg *mail.Message
+					num_batons := 5
+					batons := make(chan *bufio.Reader, num_batons)
+					for i := 0; i < num_batons; i++ {
+						batons <- new(bufio.Reader)
+					}
+					for message := range fetch_chan {
+						if m, e := mail.ReadMessage(message.GetBody(section)); e != nil {
 							continue
 						} else {
-							filename := filepath.Join(directory, f, fmt.Sprintf("msg_%d", msg.Uid))
-							fi, e := os.Create(filename)
-							if e != nil {
+							msg = m
+							header = m.Header
+							header["Folder"] = []string{folder}
+							if _, e := WriteHeaders(header, hash); e != nil {
 								panic(e)
+							} else {
+								copy(digest[:], hash.Sum(nil))
+								hash.Reset()
 							}
-
-							for _, h := range []string{
-								"From",
-								"To",
-								"Subject",
-								"In-Reply-To",
-								"References",
-								"Date",
-								"Message-ID",
-								"MIME-Version",
-								"Content-Type",
-								"Content-Disposition",
-								"Content-Transfer-Encoding",
-							} {
-								if v := message.Header.Get(h); v != "" {
-									fmt.Fprintf(fi, "%s: %s\n", h, v)
-								}
-							}
-							fmt.Fprintf(fi, "\n")
-							if _, e := io.Copy(fi, message.Body); e != nil {
-								panic(e)
-							}
-							if e := fi.Close(); e != nil {
-								panic(e)
-							}
-
 						}
+						first_byte = fmt.Sprintf("%02x", digest[0])
+						rest_bytes = fmt.Sprintf("%02x", digest[1:])
+						if i, e := os.Stat(filepath.Join(targetdir, first_byte)); e != nil {
+							if e := os.MkdirAll(filepath.Join(targetdir, first_byte), os.ModePerm); e != nil {
+								panic(e)
+							}
+						} else if !i.IsDir() {
+							panic(e)
+						}
+						if _, e := os.Stat(filepath.Join(targetdir, first_byte, rest_bytes)); e == nil {
+							continue
+						}
+						rb := <-batons
+						rb.Reset(msg.Body)
+						if g, e := os.Create(filepath.Join(targetdir, first_byte, rest_bytes)); e != nil {
+							panic(e)
+						} else {
+							go func(tf *os.File, first string, rest string, rb *bufio.Reader) {
+								if _, e := WriteMessage(header, rb, tf); e != nil {
+									panic(e)
+								} else {
+									tf.Close()
+									batons <- rb
+								}
+							}(g, first_byte, rest_bytes, rb)
+						}
+
 					}
 					donechan <- struct{}{}
 				}()
@@ -128,15 +132,72 @@ func main() {
 	}
 }
 
-// flag handler
-func parseFlags(in_flags []string) (out_flags []maildir.Flag) {
-	for _, f := range in_flags {
-		switch f {
-		case "\\Seen":
-			out_flags = append(out_flags, maildir.FlagSeen)
-		case "\\Answered":
-			out_flags = append(out_flags, maildir.FlagReplied)
+func WriteMessage(headers mail.Header, rb *bufio.Reader, w io.Writer) (n int, e error) {
+	for _, h := range header_list {
+		if v := headers.Get(h); v != "" {
+			if k, e := fmt.Fprintf(w, "%s: %s\n", h, v); e != nil {
+				return n + k, e
+			} else {
+				n += k
+			}
 		}
 	}
-	return
+	w.Write([]byte{'\n'})
+	n++
+	for {
+		if b, isPrefix, e := rb.ReadLine(); e == io.EOF || isPrefix {
+			if k, e := w.Write(b); e != nil {
+				return n + k, e
+			} else {
+				n += k
+			}
+			if e == io.EOF {
+
+				break
+			} else {
+				continue
+			}
+		} else if e != nil {
+			panic(e)
+		} else if !isPrefix {
+			if k, e := w.Write(b); e != nil {
+				return n + k, e
+			} else {
+				w.Write([]byte{'\n'})
+				n += k + 1
+			}
+		} else {
+			return n, fmt.Errorf("error writing message")
+		}
+	}
+	return n, nil
+}
+
+func WriteHeaders(headers mail.Header, w io.Writer) (n int, e error) {
+	for _, h := range header_list {
+		if v := headers.Get(h); v != "" {
+			if k, e := fmt.Fprintf(w, "%s: %s\n", h, v); e != nil {
+				return n + k, e
+			} else {
+				n += k
+			}
+		}
+	}
+	return n, nil
+}
+
+var header_list = []string{
+	"From",
+	"To",
+	"Cc",
+	"Subject",
+	"In-Reply-To",
+	"References",
+	"Date",
+	"Message-ID",
+	"MIME-Version",
+	"Content-Type",
+	"Content-Disposition",
+	"Content-Transfer-Encoding",
+	"Folder",
 }
